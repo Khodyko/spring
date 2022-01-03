3ff7423c95faaddc1f0c3eb0188afdaae4cf20fd
Igor Khodyko <Igor_Khodyko@epam.com>
2022-01-03 17:32:11 +0300

    test userService

--- src/test/java/org/example/spring/service/serviceImpl/UserServiceImplTest.java
+++ src/test/java/org/example/spring/service/serviceImpl/UserServiceImplTest.java
@@ -1,13 +1,19 @@
 package org.example.spring.service.serviceImpl;
 
 import junit.framework.TestCase;
+import org.example.spring.dao.ExceptionDao.DaoException;
 import org.example.spring.dao.daoImpl.UserDaoImpl;
 import org.example.spring.model.Entity.UserEntity;
+import org.example.spring.model.User;
+import org.example.spring.service.ServiceException.ServiceException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -28,21 +34,52 @@ public class UserServiceImplTest extends TestCase {
 
     @Test
     public void testGetUserByEmail() {
+        UserEntity userEntity=new UserEntity(12,"Sergei", "sergei@mail.ru");
+        when(userDaoImpl.getUserByEmail("sergei@mail.ru")).thenReturn(userEntity);
+        assert(userServiceImpl.getUserByEmail("sergei@mail.ru").getEmail().equals("sergei@mail.ru"));
+
     }
 
     @Test
     public void testGetUsersByName() {
+        List<User> userList=new ArrayList<>();
+        UserEntity userEntity=new UserEntity(12,"Sergei", "sergei@mail.ru");
+        userList.add(userEntity);
+        try {
+            when(userDaoImpl.getUsersByName("Sergei", 100, 0)).thenReturn(userList);
+        } catch (DaoException e) {
+            //fix me
+            e.printStackTrace();
+        }
+        List<User> usersFromMock= null;
+        try {
+            usersFromMock = userServiceImpl.getUsersByName("Sergei", 100, 0);
+        } catch (ServiceException e) {
+            //fix me
+            e.printStackTrace();
+        }
+        assertNotNull(usersFromMock.get(0));
+        assert(usersFromMock.size()>0);
     }
 
     @Test
     public void testCreateUser() {
+        UserEntity userEntity=new UserEntity(12,"Sergei", "sergei@mail.ru");
+        when(userDaoImpl.saveUser(userEntity)).thenReturn(userEntity);
+        assert(userServiceImpl.createUser(userEntity).equals(userEntity));
     }
 
     @Test
     public void testUpdateUser() {
+        UserEntity userEntity=new UserEntity(12,"Sergei", "sergei@mail.ru");
+        when(userDaoImpl.updateUser(userEntity)).thenReturn(userEntity);
+        assert(userServiceImpl.updateUser(userEntity).equals(userEntity));
     }
 
     @Test
     public void testDeleteUser() {
+        UserEntity userEntity=new UserEntity(12,"Sergei", "sergei@mail.ru");
+        when(userDaoImpl.updateUser(userEntity)).thenReturn(userEntity);
+        assert(userServiceImpl.updateUser(userEntity).equals(userEntity));
     }
 }
